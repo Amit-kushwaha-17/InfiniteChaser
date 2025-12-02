0af53567f3328397d3ed82b2f9fe47196dae483c
Amit kushwaha <amitkush615@gmail.com>
2025-12-02 21:28:06 +0530

    Random Animal Spawn
    In This Commit I Actually Set Random Animal Spawn Location, The Code Of Random Spawn Of Animal is into the SpawnManager.cs File

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float RandomXPositon = 20f;
+    private float ZPositon = 20f;
+    private float SpawnDelay = 2f;
+    private float SpawnInterval = 1.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", SpawnDelay, SpawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+    public void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-RandomXPositon, RandomXPositon), 0, ZPositon);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
+    }
+}
